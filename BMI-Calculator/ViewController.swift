3041d2335db7ed1853d15f46ebe2bd3873e43cc9
Björn Fröhling <bjorn.frohling@qvik.fi>
2016-05-25 15:43:19 +0300

    Initial Commit

---
+++ BMI-Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BMI-Calculator
+//
+//  Created by Björn Fröhling on 25/05/16.
+//  Copyright © 2016 Fröhling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
