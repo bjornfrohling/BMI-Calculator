b28604039b2820c30700fd36d96ec8416f86a9a0
Björn Fröhling <bjorn.frohling@qvik.fi>
2016-05-29 15:27:29 +0300

    UI updates. Added function for calculating BMI

--- BMI-Calculator/ViewController.swift
+++ BMI-Calculator/ViewController.swift
@@ -10,23 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var weightField: UITextField!
-    @IBOutlet weak var heightField: UITextField!
-    @IBOutlet weak var weightSlider: UISlider!
-    @IBOutlet weak var heightSlider: UISlider!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	@IBOutlet weak var weightField: UITextField!
+	@IBOutlet weak var heightField: UITextField!
+	@IBOutlet weak var weightSlider: UISlider!
+	@IBOutlet weak var heightSlider: UISlider!
+	@IBOutlet weak var outputLabel: UILabel!
 
-        title = "BMI-Calculator"
-        
-    }
+	override func viewDidLoad() {
+		super.viewDidLoad()
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+		title = "BMI-Calculator"
 
+		self.weightField.enabled = false
+		self.heightField.enabled = false
+	}
 
+	override func viewWillAppear(animated: Bool) {
+		calculateBmi()
+	}
+
+	@IBAction func didChangeWeightSlider(sender: UISlider) {
+		self.weightField.text = "\(Int(self.weightSlider.value))"
+	}
+
+	@IBAction func didChangeHeightSlider(sender: UISlider) {
+		self.heightField.text = "\(Int(self.heightSlider.value))"
+	}
+
+	@IBAction func didSelectCalculateButton(sender: UIButton) {
+		calculateBmi()
+	}
+
+	func calculateBmi() {
+		// BMI: kg/m*2
+		let kgValue = Int(weightField.text!)
+		let cmValue = Int(heightField.text!)
+
+		let sizeInMeter = Double(cmValue!) / 100.0
+		let bmi = Int(Double(kgValue!) / (sizeInMeter * sizeInMeter))
+
+		let output = "BMI: \(bmi)"
+		outputLabel.text = output
+	}
 }
 
